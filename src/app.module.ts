1a93da4e50afa158b7740de23e467b68a93e15e4
Arjun Khetia <arjun@perceptioncare.com>
2025-02-17 15:34:21 +0530

    Environment Variables Configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes config available throughout the app
+      envFilePath: '.env', // Specify the path to the .env file (default is .env)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
